68a545a5fe75ebe28c4ea902e8668e1cf169ed44
EricxLuo <ericluoo12@gmail.com>
2026-09-10 19:07:14 -0400

    fix video path

--- src/components/HobbyVideo.tsx
+++ src/components/HobbyVideo.tsx
@@ -2,7 +2,7 @@ import { motion, useScroll, useSpring, useTransform } from "motion/react";
 import { useEffect, useRef } from "react";
 import SectionDivider from "./SectionDivider";
 
-const videoSource = "/videos/editing%20collage.mp4";
+const videoSource = new URL("videos/editing%20collage.mp4", document.baseURI).toString();
 
 export default function HobbyVideo() {
   const sectionRef = useRef<HTMLElement>(null);
